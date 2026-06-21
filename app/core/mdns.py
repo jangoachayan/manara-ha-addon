7cfd48251c491a02398f80435fb4cb731209c609
Ashwin Samuel <ashwin.ashwinsamuel@gmail.com>
2026-06-21 13:29:03 +0530

    Fix mdns.py: use AsyncZeroconf to avoid EventLoopBlocked deadlock on startup

--- app/core/mdns.py
+++ app/core/mdns.py
@@ -1,12 +1,13 @@
 import socket
-from zeroconf import Zeroconf, ServiceInfo
+from zeroconf import ServiceInfo
+from zeroconf.asyncio import AsyncZeroconf
 
-def register_mdns_service() -> Zeroconf:
+async def register_mdns_service() -> AsyncZeroconf:
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect(('8.8.8.8', 80))
         local_ip = s.getsockname()[0]
 
-    zeroconf_instance = Zeroconf()
+    async_zeroconf = AsyncZeroconf()
     service_info = ServiceInfo(
         type_='_manara._tcp.local.',
         name='Manara Backend._manara._tcp.local.',
@@ -14,6 +15,6 @@ def register_mdns_service() -> Zeroconf:
         addresses=[socket.inet_aton(local_ip)],
         properties={},
     )
-    zeroconf_instance.register_service(service_info)
+    await async_zeroconf.async_register_service(service_info)
 
-    return zeroconf_instance
+    return async_zeroconf
